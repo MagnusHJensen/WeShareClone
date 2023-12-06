8abc8c6fae6e15981343be41c2c35aedaaf0853b
chros <crose19@student.sdu.dk>
2023-12-06 23:56:30 +0100

    It is now possible to view the name and email of the user that is logged in under the profile page

--- app/src/main/java/dk/sdu/weshareclone/screens/profile_screen/ProfileViewModel.kt
+++ app/src/main/java/dk/sdu/weshareclone/screens/profile_screen/ProfileViewModel.kt
@@ -2,23 +2,29 @@ package dk.sdu.weshareclone.screens.profile_screen
 
 import androidx.compose.runtime.mutableStateOf
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dk.sdu.weshareclone.model.service.AccountService
 import dk.sdu.weshareclone.model.service.ProfileService
 import dk.sdu.weshareclone.screens.WeShareViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor(private val profileService: ProfileService) : WeShareViewModel() {
+class ProfileViewModel @Inject constructor(private val profileService: ProfileService, private val accountService: AccountService) : WeShareViewModel() {
     var uiState = mutableStateOf(ProfileUiState())
         private set
 
-    private val name
-        get() = uiState.value.name
 
     private val email
         get() = uiState.value.email
 
     private val pass
         get() = uiState.value.pass
+
+    init {
+        launchCatching {
+        val currentUser = profileService.getProfile(accountService.currentUserId)
+            uiState.value = uiState.value.copy(currentUser.name, currentUser.email)
+             }
+    }
 
     fun onNameChange(newValue: String) {
         uiState.value = uiState.value.copy(name = newValue)
