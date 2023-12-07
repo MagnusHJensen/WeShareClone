3fc9d1e39f05d2d80e71959e00d7ae0531683f69
chros <crose19@student.sdu.dk>
2023-12-07 20:46:51 +0100

    Updated ProfileScreen.kt to include buttons in a row that allow you to change your name/email and go back to the home screen

--- app/src/main/java/dk/sdu/weshareclone/screens/profile_screen/ProfileScreen.kt
+++ app/src/main/java/dk/sdu/weshareclone/screens/profile_screen/ProfileScreen.kt
@@ -3,6 +3,8 @@ package dk.sdu.weshareclone.screens.profile_screen
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -12,32 +14,49 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import dk.sdu.weshareclone.HOME_SCREEN
 import dk.sdu.weshareclone.ui.theme.WeShareTheme
 
 @Composable
-fun ProfileScreen(openAndPopUp: (String, String) -> Unit, viewModel: ProfileViewModel = hiltViewModel()) {
+fun ProfileScreen(openScreen: (String) -> Unit, viewModel: ProfileViewModel = hiltViewModel()) {
     val uiState by viewModel.uiState
 
     ProfileScreenContent(
         uiState = uiState,
+        openScreen = openScreen,
         onNameChange = viewModel::onNameChange,
-        onPickName = { /*TODO*/ },
-        onEmailChange = viewModel::onEmailChange
+        onEmailChange = viewModel::onEmailChange,
+        onNameUpdate =  viewModel::onUpdateName,
+        onEmailUpdate = viewModel::onUpdateEmail
     )
 }
 
 @Composable
 fun ProfileScreenContent(
     uiState: ProfileUiState,
+    openScreen: (String) -> Unit,
     onNameChange: (String) -> Unit,
-    onPickName: () -> Unit,
-    onEmailChange: (String) -> Unit
+    onEmailChange: (String) -> Unit,
+    onNameUpdate: () -> Unit,
+    onEmailUpdate: () -> Unit
 ) {
     Column {
         Text(text = "Current name:")
         TextField(value = uiState.name, onValueChange = onNameChange, singleLine = true, modifier = Modifier.border(border = BorderStroke(width = Dp(1.0F), color = Color.Black)))
         Text(text = "Current email:")
         TextField(value = uiState.email, onValueChange = onEmailChange)
+
+        Row {
+            Button(onClick = onNameUpdate) {
+                Text(text = "Update name")
+            }
+            Button(onClick = onEmailUpdate) {
+                Text(text = "Update email")
+            }
+            Button(onClick = { openScreen(HOME_SCREEN) }) {
+                Text(text = "Back")
+            }
+        }
     }
 }
 
